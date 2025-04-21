56dd9b51388dcb2f7093e196458ad69c3500c13e
JoSalazar104 <147171327+JoSalazar104@users.noreply.github.com>
2025-04-21 10:46:26 -0500

    Delete src/preprocesing.py

--- src/preprocesing.py
+++
@@ -1,2 +0,0 @@
-df = pd.read_excel('DatosT4.xlsx') # Lee el archivo Excel 'DatosT4.xlsx' y lo almacena en un DataFrame llamado 'df'
-df2 = pd.read_excel('DatosT3.xlsx') # Lee el archivo Excel 'DatosT3.xlsx' y lo almacena en un DataFrame llamado 'df2'
